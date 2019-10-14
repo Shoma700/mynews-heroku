bc2427c1259804bbb35c75538411c9130acedce8
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-10-14 07:28:51 +0000

    13章課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,32 +1,47 @@
-<!DOCTYPE html>
-<!--
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>ProfileCreate</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
--->
-<!-- ↑10章　↓11章 -->
-
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
-
-{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@extends('layouts.admin')
 @section('title', 'プロフィールの新規作成')
 
-{{-- profile.blade.phpの＠yield('content')に以下のタグを埋め込む　--}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>　<!-- ニュース新規作成ではタイトル -->
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>　<!-- ニュース新規作成ではタイトル -->
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label> <!-- ニュース新規作成では本文で20行 -->
+                        <div class="col-md-10">
+                            <br><textarea style="display:block; box-sizing:content-box;" class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介</label> <!-- ニュース新規作成では本文で20行 -->
+                        <div class="col-md-10">
+                            <textarea style="display:block;"class="form-control" name="introduction" rows="5">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,22 @@ Route::get('/', function () {
 //↓13章
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
    Route::get('news/create', 'Admin\NewsController@add');
-   Route::post('news/create', 'Admin\NewsController@create'); # 13章追記
+   Route::post('news/create', 'Admin\NewsController@create'); //13章追記
 });
-
-
 
 //課題//
 //PHP/Laravel 10 ControllerとViewが連携できるようにしよう↓
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('profile/edit', 'Admin\ProfileController@edit'); //13章課題追加
+    Route::post('profile/edit', 'Admin\profileController@update');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/cleate', 'Admin\ProfileController@create'); //13章課題追記
 });
-//以上です。
+
+
 
 
 Auth::routes();
