e8ac0eded3281ca891c36abc082291a34f4ed4cd
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-10-18 02:06:39 +0000

    14章課題(修正１)

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\profile;
+
 class ProfileController extends Controller
 {
    
@@ -16,8 +18,23 @@ class ProfileController extends Controller
   {
       return view('admin.profile.create');
   }
-  public function create()
+  public function create(Request $request)
   {
+        //13章_Varidationを行う
+        $this->validate($request, profile::$rules);
+        $profiles = new profile;
+        $form = $request->all();
+        //フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        
+        //'name','gender','hobby','introduction'のみ保存する
+        
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        //データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+        
       return redirect('admin/profile/create');
   }
   public function update()
